853e9519f1ee32d6ada5db0d95802115abc7dfe9
Yevhen <yevhen.code@gmail.com>
2020-11-24 18:42:33 +0200

    add spinner

--- src/components/randomCocktail/randomCocktail.js
+++ src/components/randomCocktail/randomCocktail.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CocktailServices from '../../services/cocktailServices';
 import './randomCocktail.scss';
-
+import Spinner from '../spinner';
 export default class RandomCocktail extends Component {
 
     constructor() {
@@ -11,11 +11,15 @@ export default class RandomCocktail extends Component {
 
     cocktailServices = new CocktailServices();
     state = {
-        cocktail: {}
+        cocktail: {},
+        loading: true
     }
 
     onCocktailLoaded = (cocktail) => {
-        this.setState({cocktail})
+        this.setState({
+            cocktail,
+            loading: false
+        })
     }
 
     updateCocktail() {
@@ -24,29 +28,45 @@ export default class RandomCocktail extends Component {
     }
 
     render() {
-        const { cocktail: { strDrink, strDrinkThumb, strAlcoholic, strCategory, strGlass } } = this.state;
+        const { cocktail, loading } = this.state;
 
+        const spinner = loading ? <Spinner/> : null;
+        const content = !loading ? <View cocktail={cocktail}/> : null;
         return (
             <div className="random-block rounded">
-                <h4>Random Cocktail: {strDrink}</h4>
-                <div className="random-block__img">
-                    <img src={strDrinkThumb} alt={strDrink} className="rounded-sm"></img>
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Alcoholic </span>
-                        <span>{strAlcoholic}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Category </span>
-                        <span>{strCategory}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Glass </span>
-                        <span>{strGlass}</span>
-                    </li>
-                </ul>
+               
+               {spinner}
+               {content} 
+
             </div>
         );
     }
+}
+
+const View = ({cocktail}) => {
+
+    const {strDrink, strDrinkThumb, strAlcoholic, strCategory, strGlass} = cocktail;
+
+    return (
+        <>
+            <h4>Random Cocktail: {strDrink}</h4>
+            <div className="random-block__img">
+                <img src={strDrinkThumb} alt={strDrink} className="rounded-sm"></img>
+            </div>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Alcoholic </span>
+                    <span>{strAlcoholic}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Category </span>
+                    <span>{strCategory}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Glass </span>
+                    <span>{strGlass}</span>
+                </li>
+            </ul>
+        </>
+    )
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-bean-eater-etb76mn7azt"><div className="ldio-rz1j4lzgfl">
+        <div><div></div><div></div><div></div></div><div><div></div><div></div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
+
