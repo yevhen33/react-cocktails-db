ce0885a1537e915685464a790a3faaba3c41f4d0
Yevhen <yevhen.code@gmail.com>
2020-11-24 17:29:35 +0200

    connected randomCocktail to the API

--- src/components/randomCocktail/randomCocktail.js
+++ src/components/randomCocktail/randomCocktail.js
@@ -1,28 +1,49 @@
 import React, {Component} from 'react';
+import CocktailServices from '../../services/cocktailServices';
 import './randomCocktail.scss';
 
 export default class RandomCocktail extends Component {
 
+    constructor() {
+        super();
+        this.updateCocktail();
+    }
+
+    cocktailServices = new CocktailServices();
+    state = {
+        cocktail: {}
+    }
+
+    onCocktailLoaded = (cocktail) => {
+        this.setState({cocktail})
+    }
+
+    updateCocktail() {
+        this.cocktailServices.getRandomCocktail()
+            .then(this.onCocktailLoaded);
+    }
+
     render() {
+        const { cocktail: { strDrink, strDrinkThumb, strAlcoholic, strCategory, strGlass } } = this.state;
 
         return (
             <div className="random-block rounded">
-                <h4>Random Cocktail: B-52</h4>
+                <h4>Random Cocktail: {strDrink}</h4>
                 <div className="random-block__img">
-                    <img src="https://www.thecocktaildb.com/images/media/drink/xuxpxt1479209317.jpg" alt="cocktail" className="rounded-sm"></img>
+                    <img src={strDrinkThumb} alt={strDrink} className="rounded-sm"></img>
                 </div>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Alcoholic </span>
-                        <span>alcoholic</span>
+                        <span>{strAlcoholic}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Category </span>
-                        <span>Ordinary Drink</span>
+                        <span>{strCategory}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Glass </span>
-                        <span>Highball glass</span>
+                        <span>{strGlass}</span>
                     </li>
                 </ul>
             </div>
--- src/services/cocktailServices.js
+++ src/services/cocktailServices.js
@@ -1,6 +1,10 @@
-class CocktailServices {
+export default class CocktailServices {
+    constructor() {
+        this._apiBase = 'https://www.thecocktaildb.com/api/json/v1/1';
+    }
+
     async getResourses(url) {
-        let resull = await fetch(url);
+        let resull = await fetch(`${this._apiBase}${url}`);
     
         if (!resull.ok) {
             throw new Error(`Could not fetch ${url}, status ${resull.status}`);
@@ -9,12 +13,36 @@ class CocktailServices {
         return await resull.json();
     }
 
-    getRandomCocktail() {
-        return this.getResourses('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    async getRandomCocktail() {
+        const res = await this.getResourses('/random.php');
+        const {drinks} = res;
+        const cockt = drinks.map(this._transformRandomCocktail);
+        return cockt[0];
+    }
+
+    getByIngredient(id) {
+        return this.getResourses(`/filter.php?i=${id}`);
+    }
+
+    getByCategory(id) {
+        return this.getResourses(`/filter.php?c=${id}`);
+    }
+
+    getByGlass(id) {
+        return this.getResourses(`/filter.php?g=${id}`);
+    }
+
+    getFullCocktailDetails(id) {
+        return this.getResourses(`/lookup.php?i=${id}`);
+    }
+
+    _transformRandomCocktail(cocktail) {
+        return {
+            strDrink: cocktail.strDrink,
+            strDrinkThumb: cocktail.strDrinkThumb,
+            strAlcoholic: cocktail.strAlcoholic,
+            strCategory: cocktail.strCategory,
+            strGlass: cocktail.strGlass
+        } 
     }
 }
-
-const get = new CocktailServices();
-
-get.getRandomCocktail()
-.then(res => console.log(res));
