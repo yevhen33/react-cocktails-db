626d2a186e5763d4a1276aa4313f43f39c991daa
Yevhen <yevhen.code@gmail.com>
2020-11-25 22:25:32 +0200

    add toggle button for random and popular blocks

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,38 +1,81 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomCocktail from '../randomCocktail';
 import ItemList from '../itemList';
 import CocktailDetails from '../cocktailDetails';
 import PopularCocktails from '../popularCocktails';
+import ErrorMessage from '../errorMessage';
+import { Button } from 'reactstrap';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col md={{size: 5, offset: 0}}>
-                        <RandomCocktail/>
-                    </Col>
-                    <Col md={{size: 5, offset: 2}}>
-                        <PopularCocktails/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CocktailDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+export default class App extends Component {
+
+    state = {
+        showRandomCocktail: true,
+        showPopularCocktail: false,
+        error: false
+    }
+
+    toggleRandomCocktail = () => {
+        this.setState((state) => {
+            return {
+                showRandomCocktail: !state.showRandomCocktail
+            }
+        });
+    }
+
+    togglePopularCocktail = () => {
+        this.setState((state) => {
+            return {
+                showPopularCocktail: !state.showPopularCocktail
+            }
+        })
+    }
+
+    render() {
+        const {showRandomCocktail, showPopularCocktail, error} = this.state;
+
+        if(error) {
+            return <ErrorMessage/>
+        }
+
+        const randomCocktail = showRandomCocktail ? <RandomCocktail/> : null;
+        const popularCocktail = showPopularCocktail ? <PopularCocktails/> : null;
+
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col md={{size: 5, offset: 0}}>
+                            {randomCocktail}
+                            <Button 
+                                style={{ marginBottom: '1rem', 
+                                backgroundColor: 'rgba(0, 255, 127, 0.6)'}}
+                                onClick={this.toggleRandomCocktail}>Toggle random cocktail</Button>
+                        </Col>
+                        <Col md={{size: 5, offset: 2}}>
+                            {popularCocktail}
+                            <Button  
+                                style={{ marginBottom: '1rem',
+                                backgroundColor: 'rgba(0, 255, 127, 0.6)',
+                                float: 'right' }}
+                                onClick={this.togglePopularCocktail}>Toggle popular cocktail</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CocktailDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
-
-export default App;
